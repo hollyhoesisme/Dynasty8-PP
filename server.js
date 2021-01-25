9b0cab4955604d7e9da7f4b0a80483dc85d1c2da
Kim-Wilson Ngo <kimwilsonngo@gmail.com>
2021-01-24 20:18:09 -0800

    major refactor and added heroku requirements

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const todoRoutes = express.Router();
-const PORT = 4000;
+//Heroku
+const PORT = process.env.PORT || 4000;
 
 let Todo = require('./models/todo.model');
 let PropertyListings = require('./models/PropertyListings.model');
@@ -13,7 +14,8 @@ let PropertyTransfers = require('./models/PropertyTransfers.model');
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+//Heroku
+mongoose.connect(process.env.MONGOD_URI || 'mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function () {
@@ -197,6 +199,11 @@ todoRoutes.route('/deletelisting/:id').delete(function (req, res) {
 });
 
 app.use('/todos', todoRoutes);
+
+//Heroku
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../mern-todo-app.client/build'))
+}
 
 app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
